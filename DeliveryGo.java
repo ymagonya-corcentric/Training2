f3c19e39629e5a1c6da7d69ee13be9e777ea0cf1
ymagonya-corcentric <91466595+ymagonya-corcentric@users.noreply.github.com>
2022-11-27 12:48:54 +0200

    Add files via upload

---
+++ DeliveryGo.java
@@ -0,0 +1,40 @@
+public class DeliveryGo {
+
+//Реализовать код для расчета количества возможных маршрутов для доставки N клиентам
+//Использовать конструкцию do while и понятие факториала
+    //предположим N == 9
+
+    public static void main(String[] args) {
+
+        int varToGo = 9;
+        int deliveryWays = 1;
+
+        do {
+
+            // deliveryWays = deliveryWays * varToGo; varToGo--;
+
+            // deliveryWays = deliveryWays * varToGo; varToGo = varToGo - 1;
+
+            deliveryWays *= varToGo--;
+
+        } while(varToGo > 0);
+
+        System.out.println("The amount of possible delivery routes: " + varToGo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
